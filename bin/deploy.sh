515670e85232f62211b79c26f9f08e4640d40be4
pevin <elsenique.pevin@gmail.com>
2024-02-12 19:43:33 +0800

    feat: makefile command and script to deploy

---
+++ bin/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+STAGE=${STAGE:-test}
+OUTPUT_BUCKET=sam-artifacts-image-poster-api # make sure this already exist
+STACK_NAME=image-poster-api-$STAGE
+AWS_PROFILE=image-poster-api # change this to your AWS profile of your local machine
+
+sam package --profile $AWS_PROFILE --template-file template.yaml --output-template-file packaged.template.yaml --s3-bucket $OUTPUT_BUCKET
+sam deploy --profile $AWS_PROFILE --template-file packaged.template.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Stage=$STAGE --s3-bucket $OUTPUT_BUCKET

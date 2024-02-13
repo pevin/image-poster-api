9ef5d7f44efcd01ae20a98fa0ed93694fc9b76b2
pevin <elsenique.pevin@gmail.com>
2024-02-13 10:34:31 +0800

    chore: use AWS_PROFILE var in deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,7 +3,7 @@
 STAGE=${STAGE:-test}
 OUTPUT_BUCKET=sam-artifacts-image-poster-api # make sure this already exist
 STACK_NAME=image-poster-api-$STAGE
-AWS_PROFILE=image-poster-api # change this to your AWS profile of your local machine
+AWS_PROFILE=${AWS_PROFILE:-image-poster-api}
 
 sam package --profile $AWS_PROFILE --template-file template.yaml --output-template-file packaged.template.yaml --s3-bucket $OUTPUT_BUCKET
 sam deploy --profile $AWS_PROFILE --template-file packaged.template.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_AUTO_EXPAND CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides Stage=$STAGE --s3-bucket $OUTPUT_BUCKET
